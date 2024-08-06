48396a72456bbbaae10533cb7baf22000f7cc4d9
Mateus Ataide <teteulima@outlook.com>
2024-08-06 10:20:35 -0300

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,27 @@
+import { Loading } from '@/components/loading'
+import '@/styles/global.css'
+import '@/utils/dayjsLocaleConfig'
+
+import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold, useFonts } from '@expo-google-fonts/inter'
+import { Slot } from 'expo-router'
+import { StatusBar, View } from 'react-native'
+
+
+export default function Layout() {
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium,
+        Inter_600SemiBold
+    
+    })
+
+    if (!fontsLoaded) {
+        return <Loading/>
+    }
+    return (
+        <View className='bg-white flex-1 justify-center'>
+            <StatusBar barStyle='light-content' backgroundColor='transparent' translucent/>
+            <Slot/>
+        </View>
+    )
+}
