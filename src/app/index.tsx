ca071eed6febbb1f759f5846eff83d0bc9100b9d
Mateus Ataide <teteulima@outlook.com>
2024-08-06 13:56:25 -0300

    feat/Carousel: implemented Carousel

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,27 @@
+import { Loading } from "@/components/loading";
 import { Article, ArticleCarousel, newsServer } from "@/server/api";
 import { colors } from "@/styles/colors";
 import { Bell, Search } from "lucide-react-native";
 import { useEffect, useState } from "react";
-import { Dimensions, Image, View } from "react-native";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
+import Carousel from 'react-native-reanimated-carousel';
 
 //Carousel info
 const SLIDER_WIDTH = Dimensions.get('window').width;
-const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.88);  
+const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.92);  
 
 //Carousel item
 interface cardCarouselProps {
     item: {
-        image: string
+        urlToImage: string
     }
     index: number
 }
 
 function CardCarousel({ item, index }: cardCarouselProps) {
     return (
-        <View className="bg-white rounded-lg shadow-lg p-5" style={{width: ITEM_WIDTH}}>
-            <Image source={{ uri: item.image }} className="h-40 w-full rounded-lg"/>
+        <View className={`bg-white rounded-lg shadow-lg w-[${ITEM_WIDTH}]`} style={styles.cardCarousel}>
+            <Image source={{ uri: item.urlToImage }} style={styles.image} className="h-40 w-full rounded-lg"/>
         </View>
     )
 }
@@ -57,26 +59,51 @@ export default function Index() {
         fetchDataCarousel()
         fetchData()
     },[])
+
+    if (!news || !newsCarousel) {
+        return <Loading/>
+    }
     
     return (
-        <View className="flex-1 px-5">
-            <View className="flex-1 flex-row justify-between items-center">
+        <View className="flex-1 p-5">
+            <View className="flex-row justify-between items-center">
                 <Image source={require('../../assets/favicon.png')} className="h-16 w-16"/>
 
                 <View className="flex-row justify-center mt-5 gap-3">
                     <View className="rounded-full bg-zinc-300 p-3">
-                        <Bell color={colors.zinc[400]} className="h-8 w-8  "/>
+                        <Bell color={colors.zinc[400]} className="h-5 w-5  "/>
                     </View>
 
                     <View className="rounded-full bg-zinc-300 p-3">
-                        <Search color={colors.zinc[400]} className="h-8 w-8  rounded-full bg-zinc-300"/>
+                        <Search color={colors.zinc[400]} className="h-5 w-5  rounded-full bg-zinc-300"/>
                     </View>
                 </View>
             </View>
 
-            {/* <Carousel
-            
-            /> */}
+            <View style={styles.container}>
+                <Carousel
+                width={SLIDER_WIDTH}
+                autoFillData={true}
+                autoPlayInterval={3000}
+                autoPlay={true}
+                loop
+                data={newsCarousel}
+                renderItem={CardCarousel}
+                />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 10
+    },
+    cardCarousel: {
+        width: ITEM_WIDTH,
+    },
+    image: {
+        height: 200,
+    }
+})
