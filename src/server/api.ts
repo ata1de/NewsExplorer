df722a587018560f7d9392d34036cd0e04d726a9
Mateus Ataide <teteulima@outlook.com>
2024-08-06 10:36:42 -0300

    feat/ConnectServer: installed axios and created functions to get articles in api

---
+++ src/server/api.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const apiKey = process.env.NEWS_API_KEY;
+
+// Função para obter as notícias mais populares pelo nome
+async function getTopNewsByName(currentSearch: string) {
+    const options = {
+      method: 'GET',
+      url: 'https://newsapi.org/v2/everything',
+      params: {
+        q: currentSearch,
+        sortBy: 'popularity',
+        apiKey,
+      },
+    };
+  
+    try {
+      const response = await axios(options);
+      return response.data.articles;
+    } catch (error) {
+      console.error('Erro ao buscar notícias pelo nome:', error);
+      return [];
+    }
+  }
+
+// Função para obter as notícias mais populares	
+async function getTopNews() {
+    const options = {
+      method: 'GET',
+      url: 'https://newsapi.org/v2/everything',
+      params: {
+        sortBy: 'popularity',
+        apiKey,
+      },
+    };
+  
+    try {
+      const response = await axios(options);
+      return response.data.articles;
+    } catch (error) {
+      console.error('Erro ao buscar notícias:', error);
+      return [];
+    }
+}
+
+export const newsServer = { getTopNews, getTopNewsByName };
